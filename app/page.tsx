4162621fe503baf81f05ba138da226920ce1e205
Mert Can <107743711+Underprotection-TR@users.noreply.github.com>
2025-01-16 00:03:52 +0300

    Landing Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,24 @@
-import { Button } from "@/components/ui/button";
-import {
-  RegisterLink,
-  LoginLink,
-  LogoutLink,
-} from "@kinde-oss/kinde-auth-nextjs/components";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { Hero } from "./components/frontend/hero";
+import { Logos } from "./components/frontend/logos";
+import { Features } from "./components/frontend/features";
+import { PricingTable } from "./components/shared/pricing";
+import { redirect } from "next/navigation";
 
 export default async function Home() {
   const { getUser } = getKindeServerSession();
   const session = await getUser();
+
+  if (session?.id) {
+    return redirect("/dashboard");
+  }
+
   return (
-    <div className="p-10">
-      <h1>Hello World</h1>
-      {session ? (
-        <LogoutLink>
-          <Button>Log out</Button>
-        </LogoutLink>
-      ) : (
-        <div>
-          <LoginLink>
-            <Button>Login</Button>
-          </LoginLink>
-          <RegisterLink>
-            <Button>Register</Button>
-          </RegisterLink>
-        </div>
-      )}
+    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 mb-24">
+      <Hero />
+      <Logos />
+      <Features />
+      <PricingTable />
     </div>
   );
 }
